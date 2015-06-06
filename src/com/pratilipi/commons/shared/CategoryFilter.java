c34d9e1643916aa85dd936b5e3eb686553b25e1a
Rahul Ranjan <genirahul@gmail.com>
2015-06-06 14:06:55 +0530

    CategoryFilter

---
+++ src/com/pratilipi/commons/shared/CategoryFilter.java
@@ -0,0 +1,26 @@
+package com.pratilipi.commons.shared;
+
+public class CategoryFilter {
+	
+	private Long languageId;
+	
+	private CategoryType type;
+	
+	
+	public Long getLanguageId() {
+		return languageId;
+	}
+	
+	public void setLanguageId(Long languageId) {
+		this.languageId = languageId;
+	}
+	
+	public CategoryType getType() {
+		return type;
+	}
+	
+	public void setType(CategoryType type) {
+		this.type = type;
+	}
+	
+}
