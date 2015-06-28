1103ab1e65a9120283c1b20ef01d6691461c841e
Rahul Ranjan <genirahul@gmail.com>
2015-06-28 16:15:35 +0530

    Added AccessToken to put review request

--- src/com/pratilipi/pagecontent/userpratilipi/api/ReviewApi.java
+++ src/com/pratilipi/pagecontent/userpratilipi/api/ReviewApi.java
@@ -1,15 +1,16 @@
 package com.pratilipi.pagecontent.userpratilipi.api;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import com.claymus.api.GenericApi;
 import com.claymus.api.annotation.Bind;
 import com.claymus.api.annotation.Get;
 import com.claymus.api.annotation.Put;
+import com.claymus.commons.server.ClaymusHelper;
 import com.claymus.commons.shared.exception.InsufficientAccessException;
 import com.claymus.commons.shared.exception.InvalidArgumentException;
+import com.claymus.data.access.DataAccessorFactory;
+import com.claymus.data.transfer.AccessToken;
 import com.pratilipi.commons.shared.UserPratilipiFilter;
 import com.pratilipi.data.transfer.shared.UserPratilipiData;
 import com.pratilipi.pagecontent.userpratilipi.UserPratilipiContentHelper;
@@ -22,20 +23,15 @@ import com.pratilipi.pagecontent.userpratilipi.api.shared.PutReviewResponse;
 @Bind( uri = "/userpratilipi/review" )
 public class ReviewApi extends GenericApi{
 	
-	private Logger logger = Logger.getLogger( ReviewApi.class.getName() );
-
 	@Get
 	public GetReviewListResponse getReviewList( GetReviewListRequest request ){
 		
 		UserPratilipiFilter userPratilipiFilter = new UserPratilipiFilter();
 		
-		logger.log( Level.INFO, "hasPratilipiId : " + String.valueOf( request.hasPratilipiId() ) );
-		if( request.hasPratilipiId() != null && request.hasPratilipiId() )
+		if( request.hasPratilipiId() )
 			userPratilipiFilter.setPratilipiId( request.getPratilipiId() );
 		
-		logger.log( Level.INFO, "hasUserId : " + String.valueOf( request.hasUserId() ) );
-		if( request.hasUserId() != null && request.hasUserId() ){
-			logger.log( Level.INFO, "User id : " + request.getUserId() );
+		if( request.hasUserId() ){
 			userPratilipiFilter.setUserId( request.getUserId() );
 		}
 		
@@ -53,6 +49,14 @@ public class ReviewApi extends GenericApi{
 		
 		if( request.getReview() == null || request.getReview().isEmpty() )
 			return new PutReviewResponse();
+		
+		AccessToken accessToken = DataAccessorFactory.getDataAccessor( this.getThreadLocalRequest() )
+										.getAccessToken( request.getAccessToken() );
+		if( accessToken == null )
+			throw new InvalidArgumentException( "Invalid Access Token or Access Token is expired" );
+		if( accessToken.getUserId() == 0L )
+			throw new InsufficientAccessException( "User is not logged in" );
+		this.getThreadLocalRequest().setAttribute( ClaymusHelper.REQUEST_ATTRIB_ACCESS_TOKEN, accessToken );
 		
 		UserPratilipiData userPratilipiData = new UserPratilipiData();
 		userPratilipiData.setReview( request.getReview() );
--- src/com/pratilipi/pagecontent/userpratilipi/api/shared/PutReviewRequest.java
+++ src/com/pratilipi/pagecontent/userpratilipi/api/shared/PutReviewRequest.java
@@ -7,10 +7,17 @@ public class PutReviewRequest {
 	private String review;
 	
 	@Validate( required = true )
+	private String accessToken;
+	
+	@Validate( required = true )
 	private Long pratilipiId;
 	
 	public String getReview(){
 		return review;
+	}
+	
+	public String getAccessToken(){
+		return accessToken;
 	}
 	
 	public Long getPratilipiId(){
