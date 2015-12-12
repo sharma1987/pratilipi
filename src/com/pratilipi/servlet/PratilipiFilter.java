fb96f655be4481014558d28ca13c9cf39e273e5d
Rahul Ranjan <genirahul@gmail.com>
2015-12-13 00:46:40 +0530

    Redirect regex updated

--- src/com/pratilipi/servlet/PratilipiFilter.java
+++ src/com/pratilipi/servlet/PratilipiFilter.java
@@ -31,7 +31,7 @@ public class PratilipiFilter implements Filter {
 	private final Pattern validHostRegEx = Pattern.compile(
 			"(www|embed|devo|alpha)\\.pratilipi\\.com"
 			+ "|"
-			+ "((mark-4p6\\d\\.www|www)\\.prod-pratilipi|.+\\.(dev|devo)-pratilipi)\\.appspot\\.com"
+			+ "((mark-4p7\\d\\.www|www)\\.prod-pratilipi|.+\\.(dev|devo)-pratilipi)\\.appspot\\.com"
 			+ "|"
 			+ "localhost|127.0.0.1" );
 	
