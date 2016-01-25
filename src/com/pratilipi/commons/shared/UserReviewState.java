8fe90674c7ef8925a77ea88a4824b2b67ab38781
Prashant Gupta <antshpra@gmail.com>
2016-01-25 17:54:36 +0530

    Update UserReviewState.java

--- src/com/pratilipi/commons/shared/UserReviewState.java
+++ src/com/pratilipi/commons/shared/UserReviewState.java
@@ -3,11 +3,13 @@ package com.pratilipi.commons.shared;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public enum UserReviewState implements IsSerializable {
-	
-	NOT_SUBMITTED,
-	SUBMITTED,
-	APPROVED,
-	AUTO_APPROVED,
-	PUBLISHED
-		
+
+	@Deprecated
+	PENDING_APPROVAL,
+	DRAFTED,	// Review saved but not SUBMITTED or PUBLISHED
+	SUBMITTED,	// Review waiting for Moderator/System approval
+	PUBLISHED,	// Review approved by Moderator/System
+	BLOCKED,	// Review blocked by Moderator/System
+	DELETED,	// Review deleted by User OR by System when User is DELETED
+
 }
