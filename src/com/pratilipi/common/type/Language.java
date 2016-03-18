7594dda99a9af71c88f4c3807a74521afb4487f6
Rahul Ranjan <genirahul@gmail.com>
2016-03-18 17:06:11 +0530

    Kannada and telugu language enum

--- src/com/pratilipi/common/type/Language.java
+++ src/com/pratilipi/common/type/Language.java
@@ -6,9 +6,11 @@ public enum Language {
 	ENGLISH	( "en", "English", "English" ),
 	GUJARATI( "gu", "ગુજરાતી",   "Gujarati" ),
 	HINDI	( "hi", "हिंदी",      "Hindi" ),
+	KANNADA	 ( "kn", "ಕನ್ನಡ",	"Kannada" ),
 	MALAYALAM( "ml", "മലയാളം",	"Malayalam"),
 	MARATHI	( "mr", "मराठी",     "Marathi" ),
 	TAMIL	( "ta", "தமிழ்",   "Tamil" ),
+	TELUGU	( "te", "తెలుగు",   "Telugu" ),
 	;
 	
 	
