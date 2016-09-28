c0b96941d46c4a9e043bdf5c71dbd5111759b8fc
Prashant Gupta <antshpra@gmail.com>
2016-09-28 12:29:35 +0530

    Update navigation.ml

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,31 +1,28 @@
 പ്രസിദ്ധമായവ 
-/event/katha   കഥാപൂരണം
+/event/katha   കഥാപൂരണം				App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":4839232641171456 }
 /most-read     കൂടുതൽ  വായിക്കപ്പെട്ടവ     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"most-read" }
 /recommended   നിര്‍ദ്ദേശിക്കപ്പെട്ടവ           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"recommended" }
-/event/fr      ചങ്ങാതി                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":6046056954462208 }
-/event/ep      എഴുത്തുപെട്ടി             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":6207939288236032 }
 /new           പുതിയവ                 App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"new" } 
 /blog          ബ്ലോഗുകള്‍               App#BlogPostList::{ "language":"MALAYALAM", "blogId":5683739602452480 } 
+/events        മത്സരങ്ങള്‍              App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"events" }
+/classics      ക്ലാസ്സിക്സ്                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"classics" }
 
 
 വിഭാഗങ്ങൾ 
-/love          പ്രണയം                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"love" }
-/women         സ്ത്രീ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"women" }
-/memory        ഓര്‍മ്മ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"memory" }
-/mystery       ദുരൂഹം                  App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"mystery" }
-/lives         ജീവിതങ്ങള്‍               App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"lives" }
 /series        തുടര്‍ക്കഥകള്‍            App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"series" }
-/society       സമൂഹം                  App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"society" }
+/lives         ജീവിതങ്ങള്‍               App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"lives" }
+/memory        ഓര്‍മ്മ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"memory" }
+/love          പ്രണയം                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"love" }
 /satire        ആക്ഷേപഹാസ്യം         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"satire" }   
+/women         സ്ത്രീ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"women" }
+/mystery       ദുരൂഹം                  App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"mystery" }
 /letters       കത്തുകള്‍                App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"letters" }
+/society       സമൂഹം                  App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"society" }
 /non-fiction   നോണ്‍ ഫിക്ഷന്‍         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"non-fiction" }
-/classics      ക്ലാസ്സിക്സ്                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"classics" }
+
 
 സാഹിത്യ രൂപങ്ങൾ 
 /stories       കഥകൾ                App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"stories" } 
 /articles      ലേഖനങ്ങൾ             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"articles" }
 /poems         കവിതകൾ              App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"poems" }
 /books         പുസ്തകങ്ങൾ            App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"books" }
-
-
-
